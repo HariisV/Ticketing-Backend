a6f225902a38a39c0df0107afa66cf04ed9b4957
Haris Wahyudi <hariswh9@gmail.com>
2021-10-10 11:04:40 +0700

    UPLOAD

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,6 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+require("dotenv").config();
 const redis = require("redis");
 
-const client = redis.createClient();
+const client = redis.createClient({
+  host: process.env.RDS_HOST,
+  port: process.env.RDS_PORT,
+  password: process.env.RDS_PASS,
+});
 
 client.on("connect", () => {
   // eslint-disable-next-line no-console
